d6986d29b7bed315882056d9c1bf7bbf08df1ff0
monika-sulecka <monika-sulecka@wp.pl>
2025-09-03 11:42:46 +0200

    new fetch idea

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Działa to na starszej wersji node-fetch@2
+
 // http://numbersapi.com/random/year?json
 
 const fetch = require('node-fetch');
@@ -13,3 +15,28 @@ const url = `http://numbersapi.com/${year}/year?json`;
 
 console.log(year);
 console.log(url);
+
+// fetch(url)
+//     .then(res => console.log(res))
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch(err => console.log(err));
+
+// Drugi sposób importu fetch w nowszych wersjach node.js
+// (od wersji 18 fetch jest wbudowany w node.js)
+// import fetch from 'node-fetch';  
+
+// const fetch = require('node-fetch');
+
+// const year = process.argv[2] || Math.floor(Math.random() * 2020);
+// // console.log(year);
+
+fetch(`http://numbersapi.com/${year}/year?json`)
+  .then(response => {
+    console.log(response.status)
+    console.log(response.ok)
+    return response.json()
+})
+  .then(data => console.log(data.text))
+  .catch(error => console.log("Errroororor", error))
